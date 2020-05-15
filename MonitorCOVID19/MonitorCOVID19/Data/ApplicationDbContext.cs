5f53029566d716b0a2a8bd3380f38b6182961faa
Caique <caiiquexd@gmail.com>
2020-05-15 18:42:43 -0300

    mvc

--- MonitorCOVID19/MonitorCOVID19/Data/ApplicationDbContext.cs
+++ MonitorCOVID19/MonitorCOVID19/Data/ApplicationDbContext.cs
@@ -1,20 +1,15 @@
-﻿using MonitorCOVID19.Models;
-using IdentityServer4.EntityFramework.Options;
-using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 
 namespace MonitorCOVID19.Data
 {
-    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    public class ApplicationDbContext : IdentityDbContext
     {
-        public ApplicationDbContext(
-            DbContextOptions options,
-            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
         {
         }
     }

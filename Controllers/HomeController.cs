d489a24c160ed7b5461ffc9ddf8acfa957d5ed57
lucas <lucas.laborne@gmail.com>
2020-05-16 17:01:01 -0300

    senha

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -9,63 +9,73 @@ using monitor_covid19.Models;
 
 namespace monitor_covid19.Controllers
 {
-  public class HomeController : Controller
-  {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-      _logger = logger;
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        private string Senha = "abc123";
+
+        [HttpPost]
+        public IActionResult Admin(string senha)
+        {
+            Console.WriteLine("Senha: " + senha);
+            if(senha == Senha)
+                return Content($"Senha correta: {senha}");
+            else
+            {
+                ViewData["SenhaInvalida"] = true;
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Admin()
+        {
+            using (var db = new CovidContext())
+            {
+                // Create
+                // var novopais = new PaisModel();
+                // novopais.Nome = "Brasil";
+                // var novainfection = new InfeccaoModel();
+                // novainfection.CasosConfirmados = 10;
+                // novainfection.Mortes = 10;
+                // novainfection.Recuperados = 100;
+
+                // novopais.Infeccao = novainfection;
+                // db.Infeccoes.Add(novainfection);
+                // db.Paises.Add(novopais);
+                // db.SaveChanges();
+
+                // var a = db.Query<Val>("").First();
+
+                var inf = db.Infeccoes
+                            .OrderBy(b => b.InfeccaoId)
+                            .First();
+                Console.WriteLine("Pais: " + inf.Pais);
+
+            }
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
     }
-
-    public IActionResult Index()
-    {
-      return View();
-    }
-
-    public IActionResult Privacy()
-    {
-      return View();
-    }
-
-
-    public IActionResult Admin()
-    {
-      using (var db = new CovidContext())
-      {
-        // Create
-        Console.WriteLine("Add New Employee: ");
-        var novopais = new PaisModel();
-        novopais.Nome = "Brasil";
-        var novainfection = new InfeccaoModel();
-        novainfection.CasosConfirmados = 10;
-        novainfection.Mortes = 10;
-        novainfection.Recuperados = 100;
-
-        novopais.Infeccao = novainfection;
-        db.Infeccoes.Add(novainfection);
-        db.Paises.Add(novopais);
-        db.SaveChanges();
-
-        // var a = db.Query<Val>("").First();
-
-        var employee = db.Paises
-            .OrderBy(b => b.PaisId)
-            .First();
-
-        var employeee = db.Infeccoes
-                    .OrderBy(b => b.InfeccaoId)
-                    .First();
-        Console.WriteLine(employeee.Pais.Nome);
-
-      }
-      return View();
-    }
-
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-      return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
-  }
 }
